f54dbc0b1fa2db2c6dd7da8879ca63241449edb0
luckyluc-78 <luc.gerard@student-cs.fr>
2025-09-23 17:00:32 +0200

    Implement divide function and add corresponding tests

--- python_module_template/functions.py
+++ python_module_template/functions.py
@@ -17,3 +17,20 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
 
 def subtract(a, b):
     return a - b
+
+
+def divide(a, b):
+    """
+    Divide two numbers.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The division of the two numbers.
+    """
+    if a == 0 | b == 0:
+        raise Exception("Divide by zero error")
+
+    return a * b
---
+++ tests/test_divide.py
@@ -0,0 +1,15 @@
+import pytest
+from python_module_template.functions import divide
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [(2, 1, 1), (5, 2, 2.5)],
+)
+def test_divide(a, b, expected):
+    assert divide(a, b) == expected
+
+
+def test_divide_asserts():
+    assert divide(2, 1) == 1
+    assert divide(5, 2) == 2.5
