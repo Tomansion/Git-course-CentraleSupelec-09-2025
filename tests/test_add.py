082ccb6b6549fe47503cb500656757486ab029a1
Aymeric-M <aymeric.moulet@edu.ece.fr>
2025-09-23 17:12:03 +0200

    fix : add test_square_root function

--- tests/test_add.py
+++ tests/test_add.py
@@ -14,7 +14,13 @@ from python_module_template.functions import add, squareRoot
 )
 def test_add(a, b, expected):
     assert add(a, b) == expected
+
+
+def test_square_root():
     assert squareRoot(4) == 2
+    assert squareRoot(9) == 3
+    assert squareRoot(0) == 0
+    assert squareRoot(2.25) == 1.5
 
 
 def test_add_asserts():
