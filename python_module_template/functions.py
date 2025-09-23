e8ab0d093074cb8513a3827c44a9c47be9016f86
Pietro Marco GALLO <piermarco2009@gmail.com>
2025-09-23 16:57:20 +0200

    Added new operands *, /, %

--- python_module_template/functions.py
+++ python_module_template/functions.py
@@ -15,5 +15,30 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     return a + b
 
 
-def subtract(a, b):
+def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     return a - b
+
+
+def multiply(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a * b
+
+
+def power(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a**b if a != 0 and b != 0 else None
+
+
+def divide(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print(f"You're really training to divide by {b}?? What a rascal...")
+        # It's just to boost the code coverage in the tests with this exceptional case
+        return None
+
+
+def modulo(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    try:
+        return a % b
+    except ZeroDivisionError:
+        print(f"{a}%{b}... are you serious?!")
+        return None
