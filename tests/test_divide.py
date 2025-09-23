6dd7e2c647344520c187b49f0d2eaff0d7bf1cc0
luckyluc-78 <luc.gerard@student-cs.fr>
2025-09-23 17:15:45 +0200

    Update test_divide.py
    Fix inversion bug

--- tests/test_divide.py
+++ tests/test_divide.py
@@ -4,7 +4,7 @@ from python_module_template.functions import divide
 
 @pytest.mark.parametrize(
     "a, b, expected",
-    [(2, 1, 1), (5, 2, 2.5)],
+    [(2, 1, 2), (5, 2, 2.5)],
 )
 def test_divide(a, b, expected):
     assert divide(a, b) == expected
