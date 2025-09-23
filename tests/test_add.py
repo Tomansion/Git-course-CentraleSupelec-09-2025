a9dcc2d0421dd229ed652a7f916d4c5ef81bba10
tom.mansion <tomansion@yahoo.fr>
2025-09-23 15:49:46 +0200

    Added assertion tests

--- tests/test_add.py
+++ tests/test_add.py
@@ -14,3 +14,11 @@ from python_module_template.functions import add
 )
 def test_add(a, b, expected):
     assert add(a, b) == expected
+
+
+def test_add_asserts():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+    assert add(1.5, 2.5) == 4.0
+    assert add(-1.5, -2.5) == -4.0
