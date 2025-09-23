72432feb201db234ab665c35d2106cc3f4dfe124
JibLC <jbleclezio@gmail.com>
2025-09-23 17:17:54 +0200

    changed done

--- python_module_template/functions.py
+++ python_module_template/functions.py
@@ -15,5 +15,15 @@ def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     return a + b
 
 
-def subtract(a, b):
+def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]):
+    """
+    Substract 1 number to another.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The substract of the two numbers.
+    """
     return a - b
