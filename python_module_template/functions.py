26348a9f8aaa14c89276bae42801e9c295e434d1
tom.mansion <tomansion@yahoo.fr>
2025-09-24 10:33:17 +0200

    Black

--- python_module_template/functions.py
+++ python_module_template/functions.py
@@ -25,19 +25,29 @@ def add(
         return [x + y for x, y in zip(a, b)]
     elif isinstance(a, (int, float)) and isinstance(b, (int, float)):
         return a + b
+    else:
+        raise TypeError(
+            "Function parameters should be both int/float or both lists of int/float"
+        )
 
 
-def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]):
+def subtract(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
     """
-    Substract 1 number to another.
+    Subtract 1 number to another.
 
     Parameters:
     a (int or float): The first number.
     b (int or float): The second number.
 
     Returns:
-    int or float: The substract of the two numbers.
+    int or float: The subtract of the two numbers.
     """
+    if not isIntOrFloat(a) or not isIntOrFloat(b):
+        raise TypeError("Function parameters should be int or float")
+
+    return a - b
+
+
 def isIntOrFloat(vartotest):
     """
     Add an any type var.
@@ -49,11 +59,6 @@ def isIntOrFloat(vartotest):
     true if vartotest is a float or an int, false otherwise
     """
     return isinstance(vartotest, (float, int))
-
-
-def subtract(a, b):
-    # Useless comment
-    return a - b
 
 
 def squareRoot(a: Union[int, float]) -> Union[int, float]:
@@ -80,6 +85,8 @@ def power(a, b):
     if a == 0 and b == 0:
         return None
     return a**b
+
+
 def divide(a, b) -> Union[int, float]:
     """
     Divide two numbers.
